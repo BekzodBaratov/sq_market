1e9d7508804c347dd9418a65578bdfa76106009d
Bekzod <bekzodbaratov334@gmail.com>
2023-11-28 21:36:53 +0500

    test2

--- Bot/bot.js
+++ Bot/bot.js
@@ -4,7 +4,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 const web_link = "https://sq-market-client.vercel.app/";
 
 bot.start((ctx) => {
-  ctx.reply("Assalomu alaykum.\n botga xush kelibsiz \n🛒 tugmasini bosing...", {
+  ctx.reply("Assalomu alaykum.\n botga xush kelibsiz \n🛒 tugmasini bosing.", {
     reply_markup: {
       keyboard: [[{ text: "🛒", web_app: { url: web_link } }]],
     },
