748a82043f1396dfb021abed976c4b5f435e85cf
Bekzod <bekzodbaratov334@gmail.com>
2023-11-30 10:11:17 +0500

    last

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,14 +1,18 @@
-const { Telegraf } = require("telegraf");
+const { Telegraf, Markup } = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const web_link = "https://sq-market-client.vercel.app/";
 
 bot.start((ctx) => {
-  ctx.reply("Assalomu alaykum.\n botga xush kelibsiz \n🛒 tugmasini bosing.", {
-    reply_markup: {
-      keyboard: [[{ text: "🛒", web_app: { url: web_link } }]],
-    },
-  });
+  // ctx.reply("Assalomu alaykum.\n botga xush kelibsiz \n🛒 tugmasini bosing.", {
+  //   // reply_markup: {
+  //   //   keyboard: [[{ text: "🛒", web_app: { url: web_link } }]],
+  //   // },
+  // });
+  ctx.replyWithHTML(
+    "<b>Let's get started 🍟</b> \n\nPlease tap the button below to order your perfect lunch!",
+    Markup.inlineKeyboard([[Markup.button.webApp("Order Food", web_link)]])
+  );
 });
 
 bot.on("message", (ctx) => {
