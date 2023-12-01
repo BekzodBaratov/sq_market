780180d833ed9c1081b0833da74cb9e38ff55630
Bekzod <bekzodbaratov334@gmail.com>
2023-12-01 08:40:02 +0500

    last2

--- Bot/bot.js
+++ Bot/bot.js
@@ -13,25 +13,25 @@ bot.start((ctx) => {
 
 bot.on("message", (ctx) => {
   // console.log(ctx);
-  ctx.reply(JSON.stringify(ctx.message));
-  console.log(ctx.message);
-  if (ctx.message.web_app) {
-    console.log(ctx.message.web_app);
+  ctx.reply(JSON.stringify({ message: ctx.message, data___: ctx.webAppData }));
 
-    ctx.sendMediaGroup([
-      {
-        media:
-          "https://statusneo.com/wp-content/uploads/2023/02/MicrosoftTeams-image551ad57e01403f080a9df51975ac40b6efba82553c323a742b42b1c71c1e45f1.jpg",
-        type: "photo",
-        caption: "**My caption**",
-      },
-      {
-        media:
-          "https://statusneo.com/wp-content/uploads/2023/02/MicrosoftTeams-image551ad57e01403f080a9df51975ac40b6efba82553c323a742b42b1c71c1e45f1.jpg",
-        type: "photo",
-      },
-    ]);
-  }
+  // if (ctx.message.web_app) {
+  //   console.log(ctx.message.web_app);
+
+  //   ctx.sendMediaGroup([
+  //     {
+  //       media:
+  //         "https://statusneo.com/wp-content/uploads/2023/02/MicrosoftTeams-image551ad57e01403f080a9df51975ac40b6efba82553c323a742b42b1c71c1e45f1.jpg",
+  //       type: "photo",
+  //       caption: "**My caption**",
+  //     },
+  //     {
+  //       media:
+  //         "https://statusneo.com/wp-content/uploads/2023/02/MicrosoftTeams-image551ad57e01403f080a9df51975ac40b6efba82553c323a742b42b1c71c1e45f1.jpg",
+  //       type: "photo",
+  //     },
+  //   ]);
+  // }
 });
 
 module.exports = bot;
