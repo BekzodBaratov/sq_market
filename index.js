fee0c6ee12f21c067c5c841b5e1caa045d458931
Bekzod <bekzodbaratov334@gmail.com>
2023-12-01 09:41:18 +0500

    sbnt

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const bot = require("./Bot/bot");
-
-// bot.startPolling();
 
 // Unhandeled Rejection
 process.on("unhandledRejection", (err) => {
@@ -18,13 +15,9 @@ process.on("uncaughtException", (err) => {
   console.log(err.name, err.message);
   process.exit(1);
 });
-app.use(bot.webhookCallback("/webhook"));
-bot.launch().then(() => console.log("Telegram bot ishga tushdi."));
 
 require("./start/routes")(app);
 require("./start/db")();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("listening on port " + port));
-
-bot.catch((err) => console.log("Ooops", err));
